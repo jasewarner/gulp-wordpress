b5598f32e2f93d19d4352010e8cf934f4ee1fc44
Jase Warner <jase@jase.io>
2016-09-23 09:04:59 +0100

    disabled output buffering

--- functions.php
+++ functions.php
@@ -9,9 +9,6 @@
  * @link     Link
  */
 
-// Turn output buffering on.
-ob_start();
-
 require_once 'functions/func-admin.php';
 require_once 'functions/func-menu.php';
 require_once 'functions/func-permalink.php';
