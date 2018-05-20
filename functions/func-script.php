82c71da1a53204da32d816f0b95685bf8fb9c43e
Jase Warner <jase@jase.io>
2018-05-20 16:54:25 +0100

    a few tweaks

--- functions/func-script.php
+++ functions/func-script.php
@@ -24,18 +24,5 @@ function gulp_wp_theme_scripts() {
 	 * Enqueue common scripts.
 	 */
 	wp_enqueue_script( $themeHandlePrefix . '-common', get_template_directory_uri() . '/assets/js/scripts/' . $themeHandlePrefix . '-common.min.js', array( 'jquery' ), '1.0.0', true );
-
-	/**
-	 * Here is an example of how we can call a js file for a specific template.
-	 * In this case, we will enqueue home.min.js for the home page.
-	 */
-	if ( is_front_page() ) {
-
-		/**
-		 * And now we enqueue the home page scripts.
-		 */
-		wp_enqueue_script( $themeHandlePrefix . '-home', get_template_directory_uri() . '/assets/js/scripts/' . $themeHandlePrefix . '-home.min.js', array( 'jquery' ), '1.0.0', true );
-
-	}
 }
 add_action( 'wp_enqueue_scripts', 'gulp_wp_theme_scripts' );
--- functions/func-style.php
+++ functions/func-style.php
@@ -15,6 +15,5 @@
 function gulp_wp_theme_styles() {
 	wp_register_style( 'styles', get_template_directory_uri() . '/assets/css/style.min.css', array(), '1.0.0', 'all' );
 	wp_enqueue_style( 'styles' );
-
 }
 add_action( 'wp_enqueue_scripts', 'gulp_wp_theme_styles' );
