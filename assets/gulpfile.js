4abba7707dfdc1852c884f8f1e0aa66b397aa3c5
Jase Warner <jase@jase.io>
2026-08-13 12:11:11 +0100

    Updated Gulp to ESM

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,18 +1,17 @@
 'use strict';
 
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const sass = require('gulp-sass')(require('sass'));
-const cleancss = require('gulp-clean-css');
-const concat = require('gulp-concat');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const clean = require('gulp-clean');
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import cleanCss from 'gulp-clean-css';
+import concat from 'gulp-concat';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
+import autoprefixer from 'gulp-autoprefixer';
+import { deleteAsync as del } from 'del';
 
-let autoprefixer;
-const startup = async () => {
-    autoprefixer = (await import('gulp-autoprefixer')).default;
-}
+const sass = gulpSass(dartSass);
 
 /**
  * Paths and files
@@ -20,7 +19,6 @@ const startup = async () => {
 const srcScss = 'scss/**/*.scss';
 const srcJsDir = 'js';
 const srcJsCoreFiles = [
-    // `./node_modules/bootstrap/dist/js/bootstrap.js`,
     `${srcJsDir}/src/core.js`,
 ];
 const srcJsComponentFiles = [
@@ -36,26 +34,16 @@ const srcClean = [
 ];
 
 /**
- * Task to run before anything else
- */
-gulp.task('startup', async () => {
-    await startup();
-});
-
-/**
  * Clean directories containing minified assets
  */
-gulp.task('clean-dirs', () => {
-    return gulp.src(srcClean, {read: false})
-        .pipe(clean())
-});
+export const cleanDirs = () => {
+    return del(srcClean);
+};
 
 /**
- * Task for styles.
- *
- * Scss files are compiled and sent over to `assets/css/`.
+ * Core styles
  */
-gulp.task('css', gulp.series('startup', () => {
+export const css = () => {
     return gulp.src(srcScss)
         .pipe(sass.sync({
             // Suppress the "slash as division" and other API warnings
@@ -68,49 +56,55 @@ gulp.task('css', gulp.series('startup', () => {
         }).on('error', sass.logError))
         .pipe(autoprefixer({ cascade : false }))
         .pipe(rename({
-            suffix: `.min`
+            suffix: '.min'
         }))
-        .pipe(cleancss())
+        .pipe(cleanCss())
         .pipe(gulp.dest(destCss));
-}));
+};
 
 /**
- * Task for scripts.
- *
- * Js files are uglified and sent over to `assets/js/dist/`.
+ * Core scripts
  */
-gulp.task('js-core', () => {
+export const jsCore = () => {
     return gulp.src(srcJsCoreFiles)
         .pipe(babel({
-            presets : ['@babel/env']
+            presets: ['@babel/env']
         }))
-        .pipe(concat(`core.min.js`))
+        .pipe(concat('core.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(destJs));
-});
-
-gulp.task('js-components', () => {
-    return gulp.src(srcJsComponentFiles)
-        .pipe(babel({
-            presets : ['@babel/env']
-        }))
-        .pipe(rename({
-            suffix: `.min`
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest(destJs));
-});
+};
 
 /**
- * Task for watching styles and scripts.
+ * Scripts for components like blocks, post or page templates etc.
  */
-gulp.task('watch', () => {
-    gulp.watch(srcScss, gulp.series('css'));
-    gulp.watch(srcJsCoreFiles, gulp.series('js-core'));
-    gulp.watch(srcJsComponentFiles, gulp.series('js-components'));
-});
+export const jsComponents = () => {
+    return gulp.src(srcJsComponentFiles)
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest(destJs));
+};
+
+/**
+ * Combine the script tasks into one for ease
+ */
+export const js = gulp.series(jsCore, jsComponents);
+
+/**
+ * Task for watching styles and scripts
+ */
+export const watch = () => {
+    gulp.watch(srcScss, css);
+    gulp.watch(srcJsCoreFiles, jsCore);
+    gulp.watch(srcJsComponentFiles, jsComponents);
+};
 
 /**
  * Default task
  */
-gulp.task('default', gulp.series('clean-dirs', 'css', 'js-core', 'js-components') );
+export default gulp.series(cleanDirs, css, js);
